6e43b19cf5b80dccf79eeed5e6d618638d7b518d
Sarah H <sarahrose.henderson@gmail.com>
2019-09-14 14:43:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,8 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  holiday_hash.each do |season, data|
-    if season == :winter
+  holiday_hash[:winter]
     binding.pry
 end
 
